7bda92770c100114a247943c5502aa759dec58d3
abdullah-p <abdullah.pakwashee@gmail.com>
2023-10-10 11:06:29 +0100

    including original read me file

--- envs/dev-data/main.tf
+++ envs/dev-data/main.tf
@@ -14,7 +14,7 @@ resource "google_cloudfunctions_function" "function_analytics_events_transfer" {
     runtime = "python311"
     description = "function that will trigger daily transfer of GA4 data within BQ to BQ instance used for search"
     trigger_http = 1
-    https_trigger_security_level - "SECURE_ALWAYS"
+    https_trigger_security_level = "SECURE_ALWAYS"
     ingress_settings = "ALLOW_INTERNAL_ONLY"
     source_archive_bucket = google_storage_bucket.storage_analytics_transfer_function
     source_archive_object = google_storage_bucket_object.function_zipped
