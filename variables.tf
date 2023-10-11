ce0127c9db68abbcf66fbfa80d69b95c655be641
Christian Sutter <git@doublesignal.com>
2023-10-11 11:31:44 +0000

    Use REST API provider to provision Vertex resources
    This is a first attempt at provisioning Vertex over the GCP REST APIs.
    
    - Add initial resource for Vertex datastore
    - Add REST API provider and remove `gcloud` module (we can't use it)
    - Add `gcloud` tooling to devcontainer
    
    Co-authored-by: Martyn Lloyd-Kelly <mlk-kc@users.noreply.github.com>
    Co-authored-by: abdullah-p <abdullah-p@users.noreply.github.com>
    Co-authored-by: hamish-pollock-gds <hamish-pollock-gds@users.noreply.github.com>

--- variables.tf
+++ variables.tf
@@ -8,3 +8,21 @@ variable "gcp_region" {
   description = "GCP region to create non-global infrastructure in, e.g. europe-west2"
   default     = "europe-west2"
 }
+
+variable "gcp_vertex_location" {
+  type        = string
+  description = "GCP location to create Vertex Datastore instance in, e.g. global"
+  default     = "global"
+}
+
+variable "gcp_vertex_collection" {
+  type        = string
+  description = "The collection to use for Vertex Search, currently only `default_collection` is supported"
+  default     = "default_collection"
+}
+
+variable "gcp_vertex_data_store_id" {
+  type        = string
+  description = "The ID of the Vertex Datastore instance to create, e.g. search-api-v2-integration"
+  default     = "govuk-structured"
+}
