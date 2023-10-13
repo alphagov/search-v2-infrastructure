0a33ad42f512ea8ac7b2784642f129d172774f65
Christian Sutter <git@doublesignal.com>
2023-10-13 15:21:08 +0000

    Enable IAM API for project
    - Enable IAM API as this is required for the IAM resources
    - Make IAM resources depend on enabled IAM API

--- service_accounts.tf
+++ service_accounts.tf
@@ -1,19 +1,29 @@
 # Creates and configures service accounts, IAM roles, role bindings, and keys for `search-api-v2` to
 # be able to access the Discovery Engine API.
 
+# Allows managing IAM programmatically
+resource "google_project_service" "iam" {
+  project                    = var.gcp_project_id
+  service                    = "iam.googleapis.com"
+  disable_dependent_services = true
+}
+
 resource "google_service_account" "api_read" {
+  depends_on   = [google_project_service.iam]
   account_id   = "search-api-v2-read"
   display_name = "search-api-v2 (Rails API app; read only)"
   description  = "Read-only service account to provide access to the search-api-v2 Rails app"
 }
 
 resource "google_service_account" "api_write" {
+  depends_on   = [google_project_service.iam]
   account_id   = "search-api-v2-write"
   display_name = "search-api-v2 (Document Sync Worker; write only)"
   description  = "Write-only service account to provide access to the search-api-v2 Document Sync Worker"
 }
 
 resource "google_project_iam_custom_role" "api_read" {
+  depends_on  = [google_project_service.iam]
   role_id     = "search_api_v2_read"
   title       = "search-api-v2 (read only)"
   description = "Enables read-only access to the search-api-v2 Rails app"
@@ -24,6 +34,7 @@ resource "google_project_iam_custom_role" "api_read" {
 }
 
 resource "google_project_iam_custom_role" "api_write" {
+  depends_on  = [google_project_service.iam]
   role_id     = "search_api_v2_write"
   title       = "search-api-v2 (write only)"
   description = "Enabled write-only access to the search-api-v2 Document Sync Worker"
