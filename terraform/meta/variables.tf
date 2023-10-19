8fa07d369f809e271881d24da25d104d5b74fd02
Christian Sutter <git@doublesignal.com>
2023-10-19 10:39:50 +0000

    Set up GCP WI pool and ID provider for TF Cloud
    - Set up a GCP Workload Identity Pool for each environment project
    - Configure an ID provider for Terraform Cloud within each pool

--- terraform/meta/variables.tf
+++ terraform/meta/variables.tf
@@ -18,3 +18,15 @@ variable "google_cloud_billing_account" {
   type        = string
   description = "The ID of the Google Cloud billing account to associate projects with"
 }
+
+variable "tfc_hostname" {
+  type        = string
+  description = "The hostname of the Terraform Cloud/Enterprise instance to use"
+  default     = "app.terraform.io"
+}
+
+variable "tfc_organization_name" {
+  type        = string
+  description = "The name of the Terraform Cloud/Enterprise organization to use"
+  default     = "govuk"
+}
