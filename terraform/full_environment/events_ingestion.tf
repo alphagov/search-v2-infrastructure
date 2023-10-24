3d4c52d611d7eb92e69f20838862a0a5bd65feab
abdullah-p <abdullah.pakwashee@gmail.com>
2023-10-24 16:36:34 +0000

    New permissions and service account to read the source GA4 data
    Changes to main.py to test view-item

--- terraform/full_environment/events_ingestion.tf
+++ terraform/full_environment/events_ingestion.tf
@@ -1,20 +1,17 @@
-# service account for transferring data
-resource "google_service_account" "analytics_transfer" {
-  account_id   = "ga4-transfer-bq"
-  display_name = "ga4-transfer-bq"
+# service account for reading data
+resource "google_service_account" "analytics_write" {
+  account_id   = "ga4-write-bq"
+  display_name = "ga4-write-bq"
   project      = var.gcp_project_id
 }
 
 # custom role for analytics transfer
-resource "google_project_iam_custom_role" "analytics_transfer_role" {
-  role_id     = "analytics_transfer_role"
-  title       = "ga4-transfer-bq-permissions"
-  description = "Enables read-only access to the search-api-v2 Rails app"
+resource "google_project_iam_custom_role" "analytics_write_role" {
+  role_id     = "analytics_write_role"
+  title       = "ga4-write-bq-permissions"
+  description = "Write data to vertex schemas in bq"
 
   permissions = [
-    "bigquery.datasets.get",
-    "bigquery.tables.get",
-    "bigquery.tables.getData",
     "bigquery.tables.update",
     "bigquery.tables.updateData",
     "bigquery.jobs.create"
@@ -22,15 +19,48 @@ resource "google_project_iam_custom_role" "analytics_transfer_role" {
 }
 
 # binding role to service account
-resource "google_project_iam_binding" "analytics_transfer" {
+resource "google_project_iam_binding" "analytics_write" {
   project = var.gcp_project_id
-  role    = google_project_iam_custom_role.analytics_transfer_role.id
+  role    = google_project_iam_custom_role.analytics_write_role.id
 
   members = [
-    google_service_account.analytics_transfer.member
+    google_service_account.analytics_write.member
   ]
 }
 
+###
+
+# service account for reading ga4 data
+resource "google_service_account" "analytics_read" {
+  account_id   = "ga4-read-bq"
+  display_name = "ga4-read-bq"
+  project      = "GA4-Analytics"
+}
+
+# custom role for analytics read
+resource "google_project_iam_custom_role" "analytics_read_role" {
+  role_id     = "analytics_read_role"
+  title       = "ga4-read-bq-permissions"
+  description = "Read source ga4 bq event data"
+
+  permissions = [
+    "bigquery.datasets.get",
+    "bigquery.tables.get",
+    "bigquery.tables.getData"
+  ]
+}
+
+# binding role to service account
+resource "google_project_iam_binding" "analytics_read" {
+  project = "GA4-Analytics"
+  role    = google_project_iam_custom_role.analytics_read_role.id
+
+  members = [
+    google_service_account.analytics_read.member
+  ]
+}
+
+###
 # top level dataset to store events for ingestion into vertex
 resource "google_bigquery_dataset" "dataset" {
   dataset_id = "analytics_events_vertex"
@@ -56,7 +86,7 @@ resource "google_bigquery_table" "view-item-event" {
   dataset_id = google_bigquery_dataset.dataset.dataset_id
   table_id = "view-item-event"
   project = var.gcp_project_id
-  schema = file("./bq_schemas/view-item-event-schema.txt")
+  schema = file("./files/view-item-event-schema.txt")
   deletion_protection = false 
   time_partitioning {
     field = "eventTime"
@@ -67,7 +97,7 @@ resource "google_bigquery_table" "view-item-event" {
 # bucket for function .zip
 resource "google_storage_bucket" "storage_analytics_transfer_function" {
     name = "storage_analytics_transfer_function"
-    location = var.location
+    location = var.gcp_region
 }
 
 # zipped function into bucket
@@ -80,8 +110,8 @@ resource "google_storage_bucket_object" "function_zipped" {
 # archive .py and requirements.txt to zip
 data "archive_file" "init" {
   type        = "zip"
-  source_file = "./files/function/"
-  output_path = "./files/init.zip"
+  source_dir = "${path.module}/files/function/"
+  output_path = "${path.module}/files/init.zip"
 }
 
 # gen 2 function for transferring from bq - ga4 to bq - vertex events schema
@@ -91,26 +121,29 @@ resource "google_cloudfunctions2_function" "function_analytics_events_transfer" 
     description = "function that will trigger daily transfer of GA4 data within BQ to BQ instance used for search"
     location = ""
     build_config {
-        runtime = "python311"
-    }
+    runtime = "python311"
     source {
         storage_source {
             bucket = google_storage_bucket.storage_analytics_transfer_function.name
             object = google_storage_bucket_object.function_zipped.name
         }
     }
-
+    environment_variables = {
+      project_name = var.gcp_project_id,
+      dataset_name = google_bigquery_dataset.dataset.dataset_id
+    }
+    }
     service_config {
-        max_instance_count = 5,
-        vpc_connector_egress_setting = "ALL_TRAFFIC",
-        ingress_setting = "ALLOW_INTERNAL_ONLY"
+        max_instance_count = 5
+        vpc_connector_egress_settings = "ALL_TRAFFIC"
+        ingress_settings = "ALLOW_INTERNAL_ONLY"
     }
 }
 
 # 
 resource "google_service_account" "trigger_function" {
-  account_id   = "scheduler_ga4_to_bq_vertex_transfer"
-  display_name = "scheduler_ga4_to_bq_vertex_transfer"
+  account_id   = "ga4-to-vertex-transfer"
+  display_name = "ga4_to_bq_vertex_transfer"
   project      = var.gcp_project_id
 }
 
--- terraform/full_environment/files/function/main.py
+++ terraform/full_environment/files/function/main.py
@@ -11,19 +11,29 @@ def hello_http(request):
         <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
     """
     from google.cloud import bigquery
+    import os
     from concurrent.futures import ThreadPoolExecutor, as_completed
-    client = bigquery.Client(project="gds-search")
+    env_project_name = os.environ.get("project_name")
+    env_dataset_name = os.environ.get("dataset_name")
+    client = bigquery.Client(project=env_project_name)
     # Perform a query.
     QUERY = (
-        '''
-        INSERT INTO `gds-search.GA4_transfer.search-event`
-        SELECT 
-        'hello' as eventType,
-        'world' as userPseudoId,
-        'my' as eventTime,
-        [STRUCT(STRUCT('name' AS id, 'null' as name) as documentDesctiptor)] AS documents,
-        NULL as searchQuery,
-        [cast('' as string)] as pageCategories
+       
+        f'''
+        INSERT INTO `{env_project_name}.{env_dataset_name}.view-item-event`
+        SELECT
+        'view-item' AS eventType,
+            ga.user_pseudo_id AS userPseudoId,
+            FORMAT_TIMESTAMP("%FT%TZ",TIMESTAMP_MICROS(ga.event_timestamp)) AS eventTime,
+        (case when params.value.string_value is not null then [STRUCT(STRUCT(params.value.string_value AS id, CAST(NULL as string) as name) as documentDescriptor)] end) AS documents,
+        CAST(NULL as string) as searchQuery,
+        [''] as pageCategories
+        FROM `ga4-analytics-352613.analytics_330577055.events_20230603` ga,
+        UNNEST(event_params) AS params
+        WHERE
+        ga.event_name='page_view' AND
+        params.key='content_id'
+
 
         ''')
     executor = ThreadPoolExecutor(1)
