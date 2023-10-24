0a0973dec4936340db6f1710500970829bfa8bd4
abdullah-p <abdullah.pakwashee@gmail.com>
2023-10-24 13:06:42 +0000

    adding scheduler

--- terraform/full_environment/events_ingestion.tf
+++ terraform/full_environment/events_ingestion.tf
@@ -1,14 +1,14 @@
 # service account for transferring data
 resource "google_service_account" "analytics_transfer" {
-  account_id   = "ga4-read-write"
-  display_name = "ga4-read-write"
+  account_id   = "ga4-transfer-bq"
+  display_name = "ga4-transfer-bq"
   project      = var.gcp_project_id
 }
 
 # custom role for analytics transfer
 resource "google_project_iam_custom_role" "analytics_transfer_role" {
   role_id     = "analytics_transfer_role"
-  title       = "ga4-read-write-permissions"
+  title       = "ga4-transfer-bq-permissions"
   description = "Enables read-only access to the search-api-v2 Rails app"
 
   permissions = [
@@ -64,23 +64,28 @@ resource "google_bigquery_table" "view-item-event" {
     }
 }
 
+# bucket for function .zip
 resource "google_storage_bucket" "storage_analytics_transfer_function" {
     name = "storage_analytics_transfer_function"
     location = var.location
 }
 
+# zipped function into bucket
 resource "google_storage_bucket_object" "function_zipped" {
     name = "func.zip"
     bucket = google_storage_bucket.storage_analytics_transfer_function.name
     source = data.archive_file.init.output_path
 }
 
+# archive .py and requirements.txt to zip
 data "archive_file" "init" {
   type        = "zip"
   source_file = "./files/function/"
   output_path = "./files/init.zip"
 }
 
+# gen 2 function for transferring from bq - ga4 to bq - vertex events schema
+### TO DO - starting with just one schema
 resource "google_cloudfunctions2_function" "function_analytics_events_transfer" {
     name = "function_analytics_events_transfer"
     description = "function that will trigger daily transfer of GA4 data within BQ to BQ instance used for search"
@@ -102,3 +107,47 @@ resource "google_cloudfunctions2_function" "function_analytics_events_transfer" 
     }
 }
 
+# 
+resource "google_service_account" "trigger_function" {
+  account_id   = "scheduler_ga4_to_bq_vertex_transfer"
+  display_name = "scheduler_ga4_to_bq_vertex_transfer"
+  project      = var.gcp_project_id
+}
+
+# custom role for triggering transfer function
+resource "google_project_iam_custom_role" "trigger_function_role" {
+  role_id     = "trigger_function_role"
+  title       = "scheduler_ga4_to_bq_vertex_transfer-permissions"
+  description = "Enables read-only access to the search-api-v2 Rails app"
+
+  permissions = [
+    "bigquery.datasets.get"
+  ]
+}
+
+# binding role to service account
+resource "google_project_iam_binding" "trigger_function" {
+  project = var.gcp_project_id
+  role    = google_project_iam_custom_role.trigger_function_role.id
+
+  members = [
+    google_service_account.trigger_function.member
+  ]
+}
+
+#
+resource "google_cloud_scheduler_job" "daily_transfer" {
+    name = 
+    description = 
+    schedule = 
+    time_zone = 
+
+    http_target {
+        http_method = "POST"
+        uri = 
+        body = 
+        headers = {
+             "Content-Type" = "application/json"
+        }
+    }
+}
