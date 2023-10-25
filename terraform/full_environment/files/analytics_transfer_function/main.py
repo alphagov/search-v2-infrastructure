dff20ab5f828bcc1c50f98a32185afc061590784
abdullah-p <abdullah.pakwashee@gmail.com>
2023-10-25 14:59:23 +0000

    Renaming Python function and removing docstring

--- terraform/full_environment/files/analytics_transfer_function/main.py
+++ terraform/full_environment/files/analytics_transfer_function/main.py
@@ -1,14 +1,10 @@
+### TO DO
+### Docstring
+
 import functions_framework
 @functions_framework.http
-def hello_http(request):
-    """HTTP Cloud Function.
-    Args:
-        request (flask.Request): The request object.
-        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
-    Returns:
-        The response text, or any set of values that can be turned into a
-        Response object using `make_response`
-        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+def analytics_events_transfer_http(request):
+    """
     """
     from google.cloud import bigquery
     import os
