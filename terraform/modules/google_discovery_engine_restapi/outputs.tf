22c9c3782ff4b4e58d693cffd43bc80e045c2232
Christian Sutter <git@doublesignal.com>
2023-11-09 09:42:03 +0000

    Export datastore default branch into AWS SM
    This will allow us to have the app talk to the branch without having to
    manually construct the branch path in the app.

--- terraform/modules/google_discovery_engine_restapi/outputs.tf
+++ terraform/modules/google_discovery_engine_restapi/outputs.tf
@@ -3,6 +3,11 @@ output "datastore_path" {
   value       = restapi_object.discovery_engine_datastore.api_data["name"]
 }
 
+output "datastore_default_branch_path" {
+  description = "The full path of the default branch of the datastore created by the module (for data ingestion)"
+  value       = "${restapi_object.discovery_engine_datastore.api_data["name"]}/branches/default_branch"
+}
+
 output "serving_config_path" {
   description = "The serving config for the engine created by the module (for querying)"
   value       = "${restapi_object.discovery_engine_engine.api_data["name"]}/servingConfigs/default_serving_config"
