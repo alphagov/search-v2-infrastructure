9645295408f396f21b96603d7eb358d9fb52b400
abdullah-p <abdullah.pakwashee@gmail.com>
2023-11-29 11:17:00 +0000

    second table for automated test

--- terraform/full_environment/events_ingestion.tf
+++ terraform/full_environment/events_ingestion.tf
@@ -408,6 +408,25 @@ resource "google_bigquery_table" "qrels" {
 
 }
 
+resource "google_bigquery_table" "report" {
+  dataset_id          = google_bigquery_dataset.automated_evaluation_output.dataset_id
+  table_id            = "report"
+  project             = var.gcp_project_id
+  deletion_protection = false
+  external_data_configuration {
+    autodetect    = true
+    source_format = "CSV"
+    source_uris = [
+      join("", [google_storage_bucket.automated_evaluation_output.url, "*report.csv"])
+    ]
+    hive_partitioning_options {
+      mode              = "AUTO"
+      source_uri_prefix = google_storage_bucket.automated_evaluation_output.url
+    }
+  }
+
+}
+
 # # 
 # resource "google_bigquery_table" "reports" {
 #   dataset_id          = google_bigquery_dataset.automated_evaluation_output.dataset_id
