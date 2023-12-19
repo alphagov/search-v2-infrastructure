9d420d2d765df6be5e302c3082f60af629fcde7d
abdullah-p <abdullah.pakwashee@gmail.com>
2023-12-19 17:01:00 +0000

    not needed in dev environment

--- terraform/dev_environment/variables.tf
+++ terraform/dev_environment/variables.tf
@@ -3,11 +3,6 @@ variable "gcp_project_id" {
   description = "GCP Project ID of the project to create infrastructure in, e.g. search-api-v2-integration"
 }
 
-variable "gcp_project_number" {
-  type        = string
-  description = "GCP Project number"
-}
-
 variable "gcp_region" {
   type        = string
   description = "GCP region to create non-global infrastructure in, e.g. europe-west2"
