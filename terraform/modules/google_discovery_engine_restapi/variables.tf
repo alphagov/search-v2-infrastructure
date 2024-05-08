e6c8f5ce732299e9c20ed7e430c999d404f2bf68
Christian Sutter <git@doublesignal.com>
2024-05-08 09:50:19 +0000

    Add controls and serving config resources to engine
    This set of changes attempts to create the first batch of resources that
    we want to have against the datastore against the engine too.
    
    - Add engine ID var to restapi module
    - Make engine and datastore ID config dynamic from TF resource
    - Add initial controls and serving config to engine

--- terraform/modules/google_discovery_engine_restapi/variables.tf
+++ terraform/modules/google_discovery_engine_restapi/variables.tf
@@ -1,4 +1,9 @@
 variable "datastore_id" {
-  description = "The name of the datastore to create"
+  description = "The name of the datastore to create resources against"
+  type        = string
+}
+
+variable "engine_id" {
+  description = "The name of the engine to create resources against"
   type        = string
 }
